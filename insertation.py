b48dc0452ae46946aa03fe4dcf88a8243e8033ca
Muhinde234 <igirimpuhwedosta@gmail.com>
2024-04-01 11:33:43 +0200

    insertation algorithm

---
+++ insertation.py
@@ -0,0 +1,15 @@
+def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        key = arr[i]
+        j = i - 1
+        while j >= 0 and key < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = key
+
+if __name__ == "__main__":
+    # Example usage:
+    numbers = [12, 11, 13, 5, 6]
+    print("Unsorted array:", numbers)
+    insertion_sort(numbers)
+    print("Sorted array:", numbers)
